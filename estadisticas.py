1eda754a71f2bda4299b36842a444c1196a9c969
Mauricio Leguizamon <mauriciolivos17@gmail.com>
2025-11-27 08:45:11 -0500

    se creoaron demas funciones para realizar los calculos que se mostraran en la dashboard de python

--- estadisticas.py
+++ estadisticas.py
@@ -13,3 +13,39 @@ class Estadisticas:
             self.encabezado = filas[0]
             self.datos = filas[1:]
 
+    def es_numerico(self, valor):
+        try:
+            float(valor)
+            return True
+        except:
+            return False
+
+    def columnas_numericas(self):
+        numericas = []
+        for i, col in enumerate(self.encabezado):
+            if all(self.es_numerico(fila[i]) for fila in self.datos if fila[i] != ""):
+                numericas.append(i)
+        return numericas
+
+    def generar(self):
+        self.cargar()
+
+        total_filas = len(self.datos)
+        col_nums = self.columnas_numericas()
+        resumen = {}
+
+        for i in col_nums:
+            valores = [float(fila[i]) for fila in self.datos if fila[i] != ""]
+            resumen[self.encabezado[i]] = {
+                "suma": sum(valores),
+                "promedio": sum(valores) / len(valores) if valores else 0,
+                "mínimo": min(valores),
+                "máximo": max(valores),
+                "cantidad ceros": valores.count(0)
+            }
+
+        return {
+            "filas_totales": total_filas,
+            "columnas_numericas": [self.encabezado[i] for i in col_nums],
+            "resumen": resumen
+        }
